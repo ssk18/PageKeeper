1296a7e38f959e5688bc14dc2e09f762289bb7ee
Seeker1805 <kulkarnisameer1809@gmail.com>
2026-05-27 22:13:54 -0400

    Added design system icons

---
+++ core/designsystem/src/main/java/com/ssk/pagekeeper/core/designsystem/theme/Icons.kt
@@ -0,0 +1,34 @@
+package com.ssk.pagekeeper.core.designsystem.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.ssk.pagekeeper.core.designsystem.R
+
+val MenuIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_menu)
+
+val SearchIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_search)
+
+val ImportBookIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_import_book)
+
+val NavDrawerIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_nav_menu)
+
+val FavoriteIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_favorites)
+
+val LibraryIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_menu_library)
+
+val FinishIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_finish)
